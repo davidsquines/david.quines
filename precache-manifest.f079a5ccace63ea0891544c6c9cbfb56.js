a1ca0bcd586f1e48d90ebdd2927520af4140dea3
davidsquines <davidsquines@gmail.com>
2022-09-11 14:54:26 -0700

    Updates

---
+++ precache-manifest.f079a5ccace63ea0891544c6c9cbfb56.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a42365b514753a994f",
+    "url": "/david.quines/static/css/main.c6a8c0f6.chunk.css"
+  },
+  {
+    "revision": "c9a42365b514753a994f",
+    "url": "/david.quines/static/js/main.c9a42365.chunk.js"
+  },
+  {
+    "revision": "a7eb0be1e1d3a71d6052",
+    "url": "/david.quines/static/js/1.a7eb0be1.chunk.js"
+  },
+  {
+    "revision": "eae9758c3589aaf2fdf8",
+    "url": "/david.quines/static/js/runtime~main.eae9758c.js"
+  },
+  {
+    "revision": "69f0304b2cad5c1c2761bd6212574238",
+    "url": "/david.quines/static/media/DavidQuinesResume9-6.69f0304b.pdf"
+  },
+  {
+    "revision": "a3a719858d8f453d5cd9b74870849566",
+    "url": "/david.quines/static/media/profilePic.a3a71985.jpg"
+  },
+  {
+    "revision": "954b877abcbdc472fe3bb91c05966abe",
+    "url": "/david.quines/static/media/gotgains.954b877a.mp4"
+  },
+  {
+    "revision": "3bb677f13590a5df31ca7dd49d438293",
+    "url": "/david.quines/static/media/eureqavideo.3bb677f1.mp4"
+  },
+  {
+    "revision": "d635e7544bdaa1be1255cd0e7d50e7da",
+    "url": "/david.quines/static/media/chorosX.d635e754.mp4"
+  },
+  {
+    "revision": "bbb3687b36deccc35790ac859801b19a",
+    "url": "/david.quines/static/media/dictattack.bbb3687b.png"
+  },
+  {
+    "revision": "602284594ff3122c708bb813a2e195ea",
+    "url": "/david.quines/static/media/comingsoon.60228459.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/david.quines/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "430a3b71b9c806b3e5194ad1e29b1a65",
+    "url": "/david.quines/index.html"
+  }
+];
